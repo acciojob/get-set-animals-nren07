d0d37fd691b2a5cb4ab8958fef346b63d7b1466e
NARENDRA SINGH <117838092+nren07@users.noreply.github.com>
2024-03-22 07:25:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species=species;
+	}
+	get species(){
+		return this.species;
+	}
+	makeSound(logs){
+		console.log(logs);
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	let sound="woof";
+	bark(){
+		makeSound(sound);
+	}
+	
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	const sound="purr";
+	purr(){
+		makeSound(sound);
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
